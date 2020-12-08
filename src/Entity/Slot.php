517600508bc79b7a3081079157e51eab2c10bd10
Maxime Huran <m.huran@monsieurbiz.com>
2020-12-08 23:19:47 +0100

    Add slot entity classes

---
+++ src/Entity/Slot.php
@@ -0,0 +1,126 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Shipping Slot plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusShippingSlotPlugin\Entity;
+
+use DateTimeInterface;
+use Sylius\Component\Core\Model\ShipmentInterface;
+use Sylius\Component\Resource\Model\TimestampableTrait;
+
+class Slot implements SlotInterface
+{
+    use TimestampableTrait;
+
+    private ?int $id;
+    private ?DateTimeInterface $timestamp;
+    private ?int $preparationDelay;
+    private ?int $pickupDelay;
+    private ?int $durationRange;
+    private ?ShipmentInterface $shipment;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTimestamp(): ?DateTimeInterface
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTimestamp(DateTimeInterface $timetamp): void
+    {
+        $this->timestamp = $timetamp;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPreparationDelay(): ?int
+    {
+        return $this->preparationDelay;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setPreparationDelay(int $preparationDelay): void
+    {
+        $this->preparationDelay = $preparationDelay;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPickupDelay(): ?int
+    {
+        return $this->pickupDelay;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setPickupDelay($pickupDelay): void
+    {
+        $this->pickupDelay = $pickupDelay;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDurationRange(): ?int
+    {
+        return $this->durationRange;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDurationRange(int $durationRange): void
+    {
+        $this->durationRange = $durationRange;
+    }
+
+     /**
+     * {@inheritdoc}
+     */
+    public function getShipment(): ?ShipmentInterface
+    {
+        return $this->shipment;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setShipment(ShipmentInterface $shipment): void
+    {
+        $this->shipment = $shipment;
+    }
+}
---
+++ src/Entity/SlotInterface.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Shipping Slot plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusShippingSlotPlugin\Entity;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+use DateTimeInterface;
+use Sylius\Component\Core\Model\ShipmentInterface;
+use Sylius\Component\Resource\Model\TimestampableInterface;
+
+interface SlotInterface extends ResourceInterface, TimestampableInterface
+{
+    /**
+     * Get slot ID.
+     *
+     * @return int|null
+     */
+    public function getId(): ?int;
+
+    /**
+     * Set slot ID.
+     *
+     * @param int $id
+     */
+    public function setId(int $id): void;
+
+    /**
+     * Get slot timestamp.
+     *
+     * @return DateTimeInterface|null
+     */
+    public function getTimestamp(): ?DateTimeInterface;
+
+    /**
+     * Set slot timestamp.
+     *
+     * @param string $timestamp
+     */
+    public function setTimestamp(DateTimeInterface $timestamp): void;
+
+    /**
+     * Get the preparation delay in minutes.
+     *
+     * @return int|null
+     */
+    public function getPreparationDelay(): ?int;
+
+    /**
+     * Set the preparation delay in minutes.
+     *
+     * @param int $preparationDelay
+     */
+    public function setPreparationDelay(int $preparationDelay): void;
+
+    /**
+     * Get the pickup delay in minutes.
+     *
+     * @return int|null
+     */
+    public function getPickupDelay(): ?int;
+
+    /**
+     * Set the pickup delay in minutes.
+     *
+     * @param int $pickupDelay
+     */
+    public function setPickupDelay(int $pickupDelay): void;
+
+    /**
+     * Get the duration range of the slot.
+     *
+     * @return int|null
+     */
+    public function getDurationRange(): ?int;
+
+    /**
+     * Set the duration range of the slot.
+     *
+     * @param int $durationRange
+     */
+    public function setDurationRange(int $durationRange): void;
+
+    /**
+     * Get the shipment of the slot.
+     *
+     * @return ShipmentInterface|null
+     */
+    public function getShipment(): ?ShipmentInterface;
+
+    /**
+     * Set the shipment of the slot.
+     *
+     * @param ShipmentInterface $shipment
+     */
+    public function setShipment(ShipmentInterface $shipment): void;
+}
