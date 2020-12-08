bc115118128c9bb7d54b0b7aca59e6c8cf099a1e
Maxime Huran <m.huran@monsieurbiz.com>
2020-12-08 22:05:20 +0100

    Add Slot config choice form type to be able to use it in forms

---
+++ src/Form/Type/ShippingSlotConfigChoiceType.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Shipping Slot plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusShippingSlotPlugin\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\DataTransformer\CollectionToArrayTransformer;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+
+class ShippingSlotConfigChoiceType extends AbstractResourceType
+{
+    private RepositoryInterface $shippingSlotConfigRepository;
+
+    /**
+     * ShippingSlotConfigChoiceType constructor
+     *
+     * @param RepositoryInterface $shippingSlotConfigRepository
+     */
+    public function __construct(RepositoryInterface $shippingSlotConfigRepository)
+    {
+        $this->shippingSlotConfigRepository = $shippingSlotConfigRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        if ($options['multiple']) {
+            $builder->addModelTransformer(new CollectionToArrayTransformer());
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'choices' => function (Options $options): array {
+                return $this->shippingSlotConfigRepository->findAll();
+            },
+            'choice_value' => 'id',
+            'choice_label' => 'name',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent(): string
+    {
+        return ChoiceType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix(): string
+    {
+        return 'monsieurbiz_shipping_slot_config_choice';
+    }
+}
