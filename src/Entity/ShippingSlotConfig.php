f1f3f7b412856412f1c1352b4af283740f804e5c
Maxime Huran <m.huran@monsieurbiz.com>
2020-12-08 16:16:49 +0100

    Add ShippingSlotConfig classes and XML ORM

---
+++ src/Entity/ShippingSlotConfig.php
@@ -0,0 +1,139 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Shipping Slot plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusShippingSlotPlugin\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+class ShippingSlotConfig implements ShippingSlotConfigInterface
+{
+    private ?int $id;
+    private ?array $rrules;
+    private ?int $preparationDelay;
+    private ?int $pickupDelay;
+    private ?int $durationRange;
+    private ?int $availableSpots;
+    private ?string $color;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRrules(): ?array
+    {
+        return $this->rrules;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setRrules(array $rrules): void
+    {
+        $this->rrules = $rrules;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPreparationDelay(): ?int
+    {
+        return $this->preparationDelay;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setPreparationDelay(int $preparationDelay): void
+    {
+        $this->preparationDelay = $preparationDelay;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPickupDelay(): ?int
+    {
+        return $this->pickupDelay;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setPickupDelay($pickupDelay): void
+    {
+        $this->pickupDelay = $pickupDelay;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDurationRange() : ?int
+    {
+        return $this->durationRange;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDurationRange(int $durationRange): void
+    {
+        $this->durationRange = $durationRange;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAvailableSpots(): ?int
+    {
+        return $this->availableSpots;
+    }
+
+    /**
+     * {@inheritdoc}
+     */ 
+    public function setAvailableSpots(int $availableSpots): void
+    {
+        $this->availableSpots = $availableSpots;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setColor(string $color): void
+    {
+        $this->color = $color;
+    }
+}
---
+++ src/Entity/ShippingSlotConfigInterface.php
@@ -0,0 +1,118 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Shipping Slot plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusShippingSlotPlugin\Entity;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+interface ShippingSlotConfigInterface extends ResourceInterface
+{
+    /**
+     * Get shipping config ID
+     * 
+     * @return int|null
+     */
+    public function getId(): ?int;
+
+    /**
+     * Set shipping config ID
+     * 
+     * @param int $id
+     */ 
+    public function setId(int $id): void;
+
+    /**
+     * Get the list of RRULES for this config
+     * 
+     * @return string[]|null
+     */ 
+    public function getRrules(): ?array;
+
+    /**
+     * Set the list of RRULES for this config
+     * 
+     * @param string[] $rrules
+     */ 
+    public function setRrules(array $rrules): void;
+
+    /**
+     * Get the preparation delay in minutes
+     * 
+     * @return int|null
+     */ 
+    public function getPreparationDelay(): ?int;
+
+    /**
+     * Set the preparation delay in minutes
+     * 
+     * @param int $preparationDelay
+
+     */ 
+    public function setPreparationDelay(int $preparationDelay): void;
+
+    /**
+     * Get the pickup delay in minutes
+     * 
+     * @return int|null
+     */ 
+    public function getPickupDelay(): ?int;
+
+    /**
+     * Set the pickup delay in minutes
+     * 
+     * @param int $pickupDelay
+     */ 
+    public function setPickupDelay(int $pickupDelay): void;
+
+    /**
+     * Get the duration range of the slot
+     * 
+     * @return int|null
+     */ 
+    public function getDurationRange() : ?int;
+
+    /**
+     * Set the duration range of the slot
+     * 
+     * @param int $durationRange
+     */ 
+    public function setDurationRange(int $durationRange): void;
+
+    /**
+     * Get the number of available spots for a slot
+     * 
+     * @return int|null
+     */ 
+    public function getAvailableSpots(): ?int;
+
+    /**
+     * Set the number of available spots for a slot
+     * 
+     * @param int $availableSpots
+     */ 
+    public function setAvailableSpots(int $availableSpots): void;
+
+    /**
+     * Get the color displayed in calendar
+     * 
+     * @return string|null
+     */ 
+    public function getColor(): ?string;
+
+    /**
+     * Set the color displayed in calendar
+     * 
+     * @param string $color
+     */ 
+    public function setColor(string $color): void;
+}
