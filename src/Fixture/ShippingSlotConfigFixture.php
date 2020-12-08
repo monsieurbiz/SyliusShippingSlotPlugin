8c8fd126335ca8e222208ce94dbc1e6cf4a6ec5d
Maxime Huran <m.huran@monsieurbiz.com>
2020-12-08 19:13:51 +0100

    Add shipping slot config fixtures

---
+++ src/Fixture/ShippingSlotConfigFixture.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Shipping Slot plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusShippingSlotPlugin\Fixture;
+
+use Sylius\Bundle\CoreBundle\Fixture\AbstractResourceFixture;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+class ShippingSlotConfigFixture extends AbstractResourceFixture
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return 'monsieurbiz_shipping_slot_config';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureResourceNode(ArrayNodeDefinition $resourceNode): void
+    {
+        /** @phpstan-ignore-next-line */
+        $resourceNode
+            ->children()
+                ->scalarNode('name')->cannotBeEmpty()->end()
+                ->arrayNode('rrules')
+                    ->scalarPrototype()->end()
+                ->end()
+                ->integerNode('preparationDelay')->min(0)->end()
+                ->integerNode('pickupDelay')->min(0)->end()
+                ->integerNode('durationRange')->min(0)->end()
+                ->integerNode('availableSpots')->min(0)->end()
+                ->scalarNode('color')->cannotBeEmpty()->end()
+            ->end()
+        ;
+    }
+}
