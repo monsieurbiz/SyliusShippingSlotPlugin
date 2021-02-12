bb06f8afa80d023889c3cb64032cfeab982a4f48
Maxime Huran <m.huran@monsieurbiz.com>
2021-02-12 18:24:16 +0100

    Manage controller return if shipping method don't need slot to continue

--- src/Controller/SlotController.php
+++ src/Controller/SlotController.php
@@ -4,22 +4,37 @@ declare(strict_types=1);
 
 namespace MonsieurBiz\SyliusShippingSlotPlugin\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sylius\Component\Core\Repository\ShippingMethodRepositoryInterface;
 
 
 class SlotController extends AbstractController
 {
-    public function listAction()
+    private ShippingMethodRepositoryInterface $shippingMethodRepository;
+
+    public function __construct(ShippingMethodRepositoryInterface $shippingMethodRepository)
     {
+        $this->shippingMethodRepository = $shippingMethodRepository;
+    }
+
+    public function listAction(Request $request, string $code): Response
+    {
+        // Find shipping method from code
+        if (!$shippingMethod = $this->shippingMethodRepository->findOneBy(['code' => $code])) {
+            throw $this->createNotFoundException(sprintf('Shipping method "%s" not found', $code));
+        }
+
+        // No need to load slots if shipping method has no slot configuration
+        if (!$shippingMethod->getShippingSlotConfig()) {
+            return new JsonResponse(['code' => $code]);
+        }
+
         return new JsonResponse([
-            // 'code' => $uiElement->getCode(),
-            // 'form_html' => $this->renderView($uiElement->getAdminFormTemplate(), [
-            //     'form' => $form->createView(),
-            //     'uiElement' => $uiElement,
-            //     'data' => $data,
-            //     'isEdition' => (int) $isEdition,
-            // ]),
+            'code' => $code,
+            'form_html' => '',
         ]);
     }
 }
