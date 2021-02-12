aa48d2d11a527e481f35074f9a8aa57024a00287
Maxime Huran <m.huran@monsieurbiz.com>
2021-02-12 16:06:48 +0100

    Log event when we have to load shipping slots

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,33 @@
-console.log('Shipping slot Plugin JS set.');
+global.MonsieurBizShippingSlotManager = class {
+    constructor(
+        shippingMethodInputs,
+        listSlotUrl
+    ) {
+        this.shippingMethodInputs = shippingMethodInputs;
+        this.listSlotUrl = listSlotUrl;
+        this.initShippingMethodInputs();
+    }
+
+    initShippingMethodInputs() { 
+        this.shippingMethodInputs.forEach
+        for (let shippingMethodInput of this.shippingMethodInputs) {
+            // On the page load, display load slots for selected method
+            if (shippingMethodInput.checked) {
+                this.listShippingSlotsForAMethod(shippingMethodInput);
+            }
+            this.initShippingMethodInput(shippingMethodInput);
+        }
+    }
+
+    initShippingMethodInput(shippingMethodInput) { 
+        let shippingSlotManager = this;
+        shippingMethodInput.addEventListener('change', function() {
+            // On shipping method change, display load slots for selected method
+            shippingSlotManager.listShippingSlotsForAMethod(shippingMethodInput);
+        })
+    }
+
+    listShippingSlotsForAMethod(shippingMethodInput) { 
+        console.log(shippingMethodInput);
+    }
+}
