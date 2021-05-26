3a362dc74e3f661ae50ec15e0eef3faba1a59b46
Maxime Huran <m.huran@monsieurbiz.com>
2021-05-26 19:25:09 +0200

    Correct app.js lint

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,75 +1,69 @@
 global.MonsieurBizShippingSlotManager = class {
-    constructor(
-        shippingMethodInputs,
-        nextStepButtons,
-        listSlotsUrl
-    ) {
-        this.shippingMethodInputs = shippingMethodInputs;
-        this.nextStepButtons = nextStepButtons;
-        this.listSlotsUrl = listSlotsUrl;
-        this.initShippingMethodInputs();
-    }
+  constructor(shippingMethodInputs, nextStepButtons, listSlotsUrl) {
+    this.shippingMethodInputs = shippingMethodInputs;
+    this.nextStepButtons = nextStepButtons;
+    this.listSlotsUrl = listSlotsUrl;
+    this.initShippingMethodInputs();
+  }
 
-    initShippingMethodInputs() { 
-        this.shippingMethodInputs.forEach
-        for (let shippingMethodInput of this.shippingMethodInputs) {
-            // On the page load, display load slots for selected method
-            if (shippingMethodInput.checked) {
-                this.displayInputSlots(shippingMethodInput);
-            }
-            this.initShippingMethodInput(shippingMethodInput);
+  initShippingMethodInputs() {
+    this.shippingMethodInputs.forEach;
+    for (let shippingMethodInput of this.shippingMethodInputs) {
+      // On the page load, display load slots for selected method
+      if (shippingMethodInput.checked) {
+        this.displayInputSlots(shippingMethodInput);
+      }
+      this.initShippingMethodInput(shippingMethodInput);
+    }
+  }
+
+  initShippingMethodInput(shippingMethodInput) {
+    let shippingSlotManager = this;
+    shippingMethodInput.addEventListener("change", function () {
+      // On shipping method change, display load slots for selected method
+      shippingSlotManager.displayInputSlots(shippingMethodInput);
+    });
+  }
+
+  displayInputSlots(shippingMethodInput) {
+    this.disableButtons();
+    let shippingSlotManager = this;
+    this.listShippingSlotsForAMethod(shippingMethodInput.value, function () {
+      // this = req
+      if (this.status === 200) {
+        let data = JSON.parse(this.responseText);
+        // Authorize user to go to next step if no slot needed
+        if (typeof data.form_html === "undefined") {
+          shippingSlotManager.enableButtons();
+          return;
         }
-    }
 
-    initShippingMethodInput(shippingMethodInput) { 
-        let shippingSlotManager = this;
-        shippingMethodInput.addEventListener('change', function() {
-            // On shipping method change, display load slots for selected method
-            shippingSlotManager.displayInputSlots(shippingMethodInput);
-        })
-    }
+        // Display form
+        console.log(data);
+      } else {
+        shippingSlotManager.enableButtons();
+      }
+    });
+  }
 
-    displayInputSlots(shippingMethodInput) {
-        this.disableButtons();
-        let shippingSlotManager = this;
-        this.listShippingSlotsForAMethod(shippingMethodInput.value, function () {
-            // this = req
-            if (this.status === 200) {
-                let data = JSON.parse(this.responseText);
-                // Authorize user to go to next step if no slot needed
-                if (typeof data.form_html === 'undefined') { 
-                    shippingSlotManager.enableButtons();
-                    return;
-                }
+  listShippingSlotsForAMethod(shippingMethodCode, callback) {
+    let req = new XMLHttpRequest();
+    req.onload = callback;
+    let url = this.listSlotsUrl;
+    req.open("get", url.replace("__CODE__", shippingMethodCode), true);
+    req.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    req.send();
+  }
 
-                // Display form
-                console.log(data);
-            } else {
-                shippingSlotManager.enableButtons();
-            }
-        });
+  disableButtons() {
+    for (let button of this.nextStepButtons) {
+      button.disabled = true;
     }
+  }
 
-    listShippingSlotsForAMethod(shippingMethodCode, callback) { 
-        let req = new XMLHttpRequest();
-        req.onload = callback;
-        let url = this.listSlotsUrl;
-        req.open("get", url.replace('__CODE__', shippingMethodCode), true);
-        req.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-        req.send();
+  enableButtons() {
+    for (let button of this.nextStepButtons) {
+      button.disabled = false;
     }
-
-    disableButtons()
-    {
-        for (let button of this.nextStepButtons) { 
-            button.disabled = true;
-        }
-    }
-
-    enableButtons()
-    {
-        for (let button of this.nextStepButtons) { 
-            button.disabled = false;
-        }
-    }
-}
+  }
+};
