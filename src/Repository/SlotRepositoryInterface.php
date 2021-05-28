962ca02311253248f01a0121c5e019ec3e8ccf33
Maxime Huran <m.huran@monsieurbiz.com>
2021-05-28 19:09:36 +0200

    Add repository to list slots by method and date

---
+++ src/Repository/SlotRepositoryInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Shipping Slot plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusShippingSlotPlugin\Repository;
+
+use Doctrine\Persistence\ObjectRepository;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use MonsieurBiz\SyliusShippingSlotPlugin\Entity\ShippingMethodInterface;
+use DateTimeInterface;
+
+interface SlotRepositoryInterface extends ObjectRepository, RepositoryInterface
+{
+    /**
+     * @return array
+     */
+    public function findByMethodAndDate(ShippingMethodInterface $shippingMethod, ?DateTimeInterface $from = null): array;
+}
