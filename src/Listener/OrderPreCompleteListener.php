d0c4e668de775cbc5b409582791a1e4df5197a30
Maxime Huran <m.huran@monsieurbiz.com>
2021-06-07 11:48:00 +0200

    Add event to check slot integrity before place order

---
+++ src/Listener/OrderPreCompleteListener.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Shipping Slot plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusShippingSlotPlugin\Listener;
+
+use MonsieurBiz\SyliusShippingSlotPlugin\Entity\OrderInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
+use Webmozart\Assert\Assert;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
+use MonsieurBiz\SyliusShippingSlotPlugin\Entity\SlotInterface;
+use MonsieurBiz\SyliusShippingSlotPlugin\Generator\SlotGeneratorInterface;
+
+final class OrderPreCompleteListener
+{
+    private RouterInterface $router;
+    private EntityManagerInterface $slotManager;
+    private SlotGeneratorInterface $slotGenerator;
+
+    public function __construct(
+        RouterInterface $router,
+        EntityManagerInterface $slotManager,
+        SlotGeneratorInterface $slotGenerator
+    ) {
+        $this->router = $router;
+        $this->slotManager = $slotManager;
+        $this->slotGenerator = $slotGenerator;
+    }
+
+    public function checkSlot(ResourceControllerEvent $event): void
+    {
+        $order = $event->getSubject();
+
+        /** @var OrderInterface $order */
+        Assert::isInstanceOf($order, OrderInterface::class);
+
+        $nonValidSlots = [];
+        /** @var SlotInterface $slot */
+        foreach ($order->getSlots() as $slot) {
+            if (!$slot->isValid() || $this->slotGenerator->isFull($slot)) {
+                $nonValidSlots[] = $slot;
+                $this->slotManager->remove($slot);
+            }
+        }
+
+        if (empty($nonValidSlots)) {
+            return;
+        }
+
+        $this->slotManager->flush();
+        $event->stop(
+            'monsieurbiz_shipping_slot.order.slot_no_longer_available',
+            ResourceControllerEvent::TYPE_ERROR
+        );
+        $event->setResponse(new RedirectResponse($this->router->generate('sylius_shop_cart_summary')));
+    }
+}
