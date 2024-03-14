c1038f45e954bb024d0c44ad7d760978e3fa4f66
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2024-03-14 15:24:36 +0100

    feat: display multiple shipping slot in shipping method form

--- src/Form/Extension/ShippingMethodTypeExtension.php
+++ src/Form/Extension/ShippingMethodTypeExtension.php
@@ -16,6 +16,7 @@ namespace MonsieurBiz\SyliusShippingSlotPlugin\Form\Extension;
 use MonsieurBiz\SyliusShippingSlotPlugin\Form\Type\ShippingSlotConfigChoiceType;
 use Sylius\Bundle\ShippingBundle\Form\Type\ShippingMethodType;
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -32,10 +33,11 @@ class ShippingMethodTypeExtension extends AbstractTypeExtension
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('shippingSlotConfig', ShippingSlotConfigChoiceType::class, [
+            ->add('shippingSlotConfigs', ShippingSlotConfigChoiceType::class, [
                 'required' => false,
-                'placeholder' => 'monsieurbiz_shipping_slot.ui.no_shipping_slot_config',
                 'label' => 'monsieurbiz_shipping_slot.ui.form.shipping_slot_config',
+                'multiple' => true,
+                'expanded' => true,
                 'constraints' => [
                     new Assert\Valid(),
                 ],
