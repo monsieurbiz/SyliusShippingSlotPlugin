2d6ed365edd59fbbbc035b40051ecf2c94516990
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2024-03-15 15:41:57 +0100

    fix: execute plugin migration before sylius core migration

--- src/DependencyInjection/MonsieurBizSyliusShippingSlotExtension.php
+++ src/DependencyInjection/MonsieurBizSyliusShippingSlotExtension.php
@@ -13,13 +13,17 @@ declare(strict_types=1);
 
 namespace MonsieurBiz\SyliusShippingSlotPlugin\DependencyInjection;
 
+use Sylius\Bundle\CoreBundle\DependencyInjection\PrependDoctrineMigrationsTrait;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-final class MonsieurBizSyliusShippingSlotExtension extends Extension
+final class MonsieurBizSyliusShippingSlotExtension extends Extension implements PrependExtensionInterface
 {
+    use PrependDoctrineMigrationsTrait;
+
     public function load(array $config, ContainerBuilder $container): void
     {
         $configuration = $this->processConfiguration($this->getConfiguration([], $container), $config);
@@ -31,5 +35,27 @@ final class MonsieurBizSyliusShippingSlotExtension extends Extension
     public function getAlias(): string
     {
         return str_replace('monsieur_biz', 'monsieurbiz', parent::getAlias());
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->prependDoctrineMigrations($container);
+    }
+
+    protected function getMigrationsNamespace(): string
+    {
+        return 'MonsieurBiz\SyliusShippingSlotPlugin\Migrations';
+    }
+
+    protected function getMigrationsDirectory(): string
+    {
+        return '@MonsieurBizSyliusShippingSlotPlugin/Migrations';
+    }
+
+    protected function getNamespacesOfMigrationsExecutedBefore(): array
+    {
+        return [
+            'Sylius\Bundle\CoreBundle\Migrations',
+        ];
     }
 }
